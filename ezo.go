e331451e3bb830c98ff2711f1aa3b3badd13af5d
Ranjib Dey <ranjib@linux.com>
2018-02-19 22:16:58 -0800

    add command details

--- ezo.go
+++ ezo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/reef-pi/rpi/i2c"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,31 +13,48 @@ https://www.atlas-scientific.com/_files/_datasheets/_circuit/pH_EZO_datasheet.pd
 */
 
 type AtlasEZO struct {
-	addr byte
-	bus  i2c.Bus
+	addr  byte
+	bus   i2c.Bus
+	delay time.Duration
 }
-
-type EZO_STATE byte
-
-const EZO_PAUSE_TIME = 300 * time.Millisecond
-
-const (
-	EZO_OFF EZO_STATE = iota
-	EZO_ON
-)
 
 func NewAtlasEZO(addr byte, bus i2c.Bus) *AtlasEZO {
 	return &AtlasEZO{
-		addr: addr,
-		bus:  bus,
+		addr:  addr,
+		bus:   bus,
+		delay: time.Second,
 	}
+}
+
+func (a *AtlasEZO) extractIntResponse() (int, error) {
+	resp, err := a.read()
+	if err != nil {
+		return 0, err
+	}
+	parts := strings.Split(resp, ",")
+	if len(parts) != 2 {
+		return 0, fmt.Errorf("Malformed response:'%s'", resp)
+	}
+	return strconv.Atoi(parts[1])
+}
+
+func (a *AtlasEZO) extractFloatResponse() (float64, error) {
+	resp, err := a.read()
+	if err != nil {
+		return 0, err
+	}
+	parts := strings.Split(resp, ",")
+	if len(parts) != 2 {
+		return 0, fmt.Errorf("Malformed response:'%s'", resp)
+	}
+	return strconv.ParseFloat(parts[1], 64)
 }
 
 func (a *AtlasEZO) command(cmd string) error {
 	if err := a.bus.WriteBytes(a.addr, []byte(cmd+"\000")); err != nil {
 		return err
 	}
-	time.Sleep(EZO_PAUSE_TIME)
+	time.Sleep(a.delay)
 	return nil
 }
 
@@ -45,13 +63,30 @@ func (a *AtlasEZO) read() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	fmt.Println(payload)
-	fmt.Println(payload[0])
-	fmt.Println(string(payload[1:]))
 	if payload[0] != byte(1) {
-		//	return "", fmt.Errorf("Failed to execute. Error:%s", string(payload))
+		return "", fmt.Errorf("Failed to execute. Error:%s", string(payload))
 	}
-	return string(payload[1:]), nil
+	p := strings.Trim(string(payload[1:]), "\000")
+	return p, nil
+}
+
+func (a *AtlasEZO) LedOn() error {
+	return a.command("L,1")
+}
+
+func (a *AtlasEZO) LedOff() error {
+	return a.command("L,0")
+}
+
+func (a *AtlasEZO) LedState() (bool, error) {
+	if err := a.command("L,?"); err != nil {
+		return false, err
+	}
+	i, err := a.extractIntResponse()
+	if err != nil {
+		return false, err
+	}
+	return i == 1, nil
 }
 
 func (a *AtlasEZO) Baud(n int) error {
@@ -86,8 +121,11 @@ func (a *AtlasEZO) ClearCalibration() error {
 	return a.command("Cal,clear")
 }
 
-func (a *AtlasEZO) IsCalibrated() error {
-	return a.command("Cal,?")
+func (a *AtlasEZO) IsCalibrated() (int, error) {
+	if err := a.command("Cal,?"); err != nil {
+		return 0, err
+	}
+	return a.extractIntResponse()
 }
 
 func (a *AtlasEZO) Export() error {
@@ -106,24 +144,19 @@ func (a *AtlasEZO) Find() error {
 	return a.command("Find")
 }
 
-func (a *AtlasEZO) Information() error {
-	return a.command("i")
-}
-
-func (a *AtlasEZO) ChangeI2CAddress() error {
-	return nil
-}
-
-func (a *AtlasEZO) SetLed(e EZO_STATE) error {
-	return a.command(string([]byte{byte(e)}))
-}
-
-func (a *AtlasEZO) GetLed() error {
-	return a.command("L,?")
-}
-
-func (a *AtlasEZO) ProtocolLock() error {
-	return nil
+func (a *AtlasEZO) Information() (string, string, error) {
+	if err := a.command("i"); err != nil {
+		return "", "", err
+	}
+	resp, err := a.read()
+	if err != nil {
+		return "", "", err
+	}
+	parts := strings.Split(resp, ",")
+	if len(parts) != 3 {
+		return "", "", fmt.Errorf("Malformed response:%s", resp)
+	}
+	return parts[1], parts[2], nil
 }
 
 func (a *AtlasEZO) Read() (float64, error) {
@@ -145,10 +178,29 @@ func (a *AtlasEZO) Slope() error {
 	return nil
 }
 
-func (a *AtlasEZO) Status() error {
-	return a.command("Status")
+func (a *AtlasEZO) Status() (string, string, error) {
+	if err := a.command("Status"); err != nil {
+		return "", "", err
+	}
+	//?Status,P,5.038
+	resp, err := a.read()
+	if err != nil {
+		return "", "", err
+	}
+	parts := strings.Split(resp, ",")
+	if len(parts) != 3 {
+		return "", "", fmt.Errorf("Malformed response:'%s'", resp)
+	}
+	return parts[1], parts[2], nil
 }
 
-func (a *AtlasEZO) TemperatureCompensate() error {
-	return nil
+func (a *AtlasEZO) GetTC() (float64, error) {
+	if err := a.command("T,?"); err != nil {
+		return 0, err
+	}
+	return a.extractFloatResponse()
+}
+
+func (a *AtlasEZO) SetTC(t float64) error {
+	return a.command(fmt.Sprintf("T,%f", t))
 }
--- ezo_test.go
+++ ezo_test.go
@@ -8,12 +8,46 @@ import (
 func TestEZO(t *testing.T) {
 	bus := i2c.MockBus()
 	e := NewAtlasEZO(byte(0x93), bus)
-	bus.Bytes = []byte("19.65")
+	e.delay = 0
+	bus.Bytes = append([]byte{1}, []byte("9.65")...)
 	if _, err := e.Read(); err != nil {
 		t.Error(err)
 	}
-	bus.Bytes = []byte("09.65")
+	bus.Bytes = append([]byte{0}, []byte("9.65")...)
 	if _, err := e.Read(); err == nil {
 		t.Error("Values starting with 0 should fail")
 	}
+
+	bus.Bytes = append([]byte{1}, []byte("L,1")...)
+	on, err := e.LedState()
+	if err != nil {
+		t.Error(err)
+	}
+	if !on {
+		t.Error("Expected led on, returned off")
+	}
+
+	bus.Bytes = append([]byte{1}, []byte("?T,19.5")...)
+	v, err := e.GetTC()
+	if err != nil {
+		t.Error(err)
+	}
+	if v != 19.5 {
+		t.Error("Expected 19.5 . Found:", v)
+	}
+
+	bus.Bytes = append([]byte{1}, []byte("?i,pH,2.8")...)
+	d, i, err := e.Information()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if d != "pH" {
+		t.Error("Expected device pH. Found:", d)
+	}
+
+	if i != "2.8" {
+		t.Error("Expected version 2.8. Found:", i)
+	}
+
 }
